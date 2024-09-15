abe17e0e329484a875eb91533ae1f216130a2c6a
Hamza <59485770+Crepco@users.noreply.github.com>
2024-09-15 22:09:43 +0530

    basic structure

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+// Basic JavaScript file
+
+// This function will be called when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("StampHub frontend is loaded.");
+
+});
